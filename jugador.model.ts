369e89309be5e2870fe702ba9568a33f2fc87abb
martaa163 <74718915+martaa163@users.noreply.github.com>
2021-11-30 20:56:54 +0100

    Update jugador.model.ts

--- jugador.model.ts
+++ jugador.model.ts
@@ -4,7 +4,7 @@ export class Jugador {
     nivel: string;
     partidasApuntado: Partida[];
 
-    constructor(nombre:string, apellido: string, nivel:string){
+    constructor(nombre:string, apellido: string, nivel:string, partida:Partida){
 
         this.nombre = nombre;
         this.apellido = apellido;
@@ -22,11 +22,30 @@ export class Jugador implements Deserializable {
     nombre: string;
     apellido: string;
     nivel: string;
-    partidasApuntado: Partida[];
+    partida: Partida;
+    
+    constructor(nombre:string, apellido: string, nivel:string, partida:Partida){
 
-  deserialize(input: any) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nivel = nivel;
+        this.partida = Partida;
+     }
+
+   deserialize(input: any) {
     Object.assign(this, input);
     return this;
-  }
+   }  
+    
+}
+
+class listaJugadores{
+    jugadores: Jugador[];
+    
+    constructor(){
+        this.jugadores=[ // news de jugadores ]; 
+    }
+}
+
 }
 
