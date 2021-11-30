92ad542f659f45f2e53a3a9fe022b21811ecc336
martaa163 <74718915+martaa163@users.noreply.github.com>
2021-11-30 20:12:36 +0100

    Update deserializable.model.ts

--- deserializable.model.ts
+++ deserializable.model.ts
@@ -2,4 +2,7 @@
 
 export interface Deserializable {
     deserialize(input: any): this;
+    this.partidasApuntado = new Partida().deserialize(input.partidasApuntado); 
+    this.jugador = new Jugador().deserialize(input.jugador); 
+    return this;
   }
